fa49ae544da105e5104ff07e5526a635d0967a58
kevin <kwrjcr@gmail.com>
2016-03-01 13:27:23 -0800

    The line I put in place is incorrect, modified the file.

--- docker-ubuntu-upstart/script_base/build_default_pack.sh
+++ docker-ubuntu-upstart/script_base/build_default_pack.sh
@@ -5,7 +5,7 @@ source /root/script_base.sh
 
 # REPOS
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv-keys 0x4f4ea0aae5267a6c
-echo "deb http://ppa.launchpad.net/ondrej/php" > /etc/apt/sources.list.d/php7.list
+echo "deb http://ppa.launchpad.net/ondrej/php/ubuntu trusty main" > /etc/apt/sources.list.d/php7.list
 
 # php5.6
 sudo add-apt-repository ppa:ondrej/php5-5.6 -y
