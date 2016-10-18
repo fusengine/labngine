dc3d21a2128a9dc3ab2749b1066b44e991d9394f
Bruno Azoulay <azoulaybruno@gmail.com>
2016-10-18 11:10:27 +0200

    add ubuntu xenial clean

---
+++ docker-ubuntu/ubuntu-xenial-clean/script_base/build_default_pack.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# add script function
+source /root/script_base.sh
+
+# add packages
+PACKAGES_DEFAULT="vim curl sudo wget git zip unzip htop supervisor build-essential \
+                  software-properties-common python-software-properties language-pack-en-base"
+
+# Env package to dockerfile
+PACKAGES_ENV_DOCKERFILES=${PACKAGES_BUILDPACK}
+
+# Update Ubuntu
+update
+
+# Install_packages
+install_packages
+
+# Upgrade ubuntu
+upgrade
+
+# Clean ubuntu
+clean_ubuntu
